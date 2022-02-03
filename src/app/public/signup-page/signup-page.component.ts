3cbf49279d24b9b7b908e0cda15386f0158c8a76
Michelle Kênia <keniamsoaress@gmail.com>
2022-02-02 21:13:27 -0300

    Criar pagina de signup

--- src/app/public/signup-page/signup-page.component.ts
+++ src/app/public/signup-page/signup-page.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Router } from '@angular/router';
+import { CrudServiceService } from 'src/app/shared/services/crud-service.service';
 
 @Component({
   selector: 'app-signup-page',
@@ -7,9 +10,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupPageComponent implements OnInit {
 
-  constructor() { }
+  form: FormGroup = new FormGroup({});
+
+  constructor(
+    private crudService: CrudServiceService,
+    private route: Router) { }
 
   ngOnInit(): void {
+  }
+
+  signup(){
+    return
   }
 
 }
